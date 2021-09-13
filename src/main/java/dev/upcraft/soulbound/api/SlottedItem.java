dc66c24c758c88323832d57cdc0aec5c8d12d211
Joshua Schirra <j-schirra@gmx.de>
2021-09-13 03:48:29 +0200

    final round of cleanup

--- src/main/java/dev/upcraft/soulbound/api/SlottedItem.java
+++
@@ -1,7 +0,0 @@
-package dev.upcraft.soulbound.api;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
-
-public record SlottedItem(Identifier containerId, ItemStack stack, int slotId) {
-}
