7672ba6f0d78886d4658677cf248d084aa12ca03
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2022-11-20 00:52:41 +0100

    reimplemented all modmenu integration.

--- src/main/java/dev/upcraft/soulbound/compat/ModmenuIntegration.java
+++ src/main/java/dev/upcraft/soulbound/compat/ModmenuIntegration.java
@@ -1,17 +1,17 @@
 package dev.upcraft.soulbound.compat;
 
-// import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-// import com.terraformersmc.modmenu.api.ModMenuApi;
-//import dev.upcraft.soulbound.core.SoulboundConfig;
-//import me.shedaniel.autoconfig.AutoConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.upcraft.soulbound.core.SoulboundConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class ModmenuIntegration {//implements ModMenuApi {
+public class ModmenuIntegration implements ModMenuApi {
 
-    // @Override
-    // public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    //     return parent -> AutoConfig.getConfigScreen(SoulboundConfig.class, parent).get();
-    // }
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(SoulboundConfig.class, parent).get();
+    }
 }
